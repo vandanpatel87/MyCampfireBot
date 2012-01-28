abd97301ba079f310a06023b54ebb6ddd64d63de
= <=>
2012-01-27 16:21:54 -0800

    Friday's text commit

--- lib/campfire_bot/plugins/text.rb
+++ lib/campfire_bot/plugins/text.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'twilio-ruby'
 
+
 class Text < CampfireBot::Plugin
   on_command 'text', :text_method
   
@@ -11,9 +12,13 @@ class Text < CampfireBot::Plugin
     number = out.shift
     msg.speak("Destination Number : " + number)
     
-    message = out.join(" ")
-    msg.speak("Message Sent: " + message)
-    
+    sms_body = out.join(" ")
+    msg.speak("Message Sent: " + sms_body)
+    deliver_message(sms_body,number)
+  end
+  
+ def deliver_message(sms_body,number)
+   
     account_sid = 'ACd2900b750a9343ff4348feae326a8ff7'
     auth_token = 'adce9bd993361bae6b40647d918c8e72'
     
@@ -22,8 +27,8 @@ class Text < CampfireBot::Plugin
     @client.account.sms.messages.create(
       :from => '+16504926856',
       :to => '+1' + number,
-      :body => message + "\r\n" + "BookRenter.com"
-    )
- 
-  end 
+      :body => sms_body 
+    )  
+  end
 end
+ 
